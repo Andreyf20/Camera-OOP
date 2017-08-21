bec21ff6ade571934995ca5f664416f78e122376
Andrey Torres Calderón <atorresf20@gmail.com>
2017-08-21 01:23:26 -0600

    Now it saves the picture taken
    added a function to save the picture, decode the bitmap to show the picture on the imageview.

--- InstaPOO/app/src/main/java/com/andrey/instapoo/MainActivity.java
+++ InstaPOO/app/src/main/java/com/andrey/instapoo/MainActivity.java
@@ -4,23 +4,35 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSIONS_REQUEST_CODE = 1;
+    private static final int CAMERA_REQUEST_CODE = 1;
     public static ImageView imageview;
     public static Button takepicture;
+    String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +40,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageview = (ImageView)findViewById(R.id.imageView);
         takepicture = (Button)findViewById(R.id.takepicture);
-
         this.checkAndRequestPermissions();
     }
 
     public void takepic(View view){
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, 1);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+            // Aqui es donde lo habia puesto y medio funcionaban las dos cosas
+            File photofile = null;
+            try{
+                photofile = createImageFile();
+            }catch (IOException e){}
+            if(photofile != null){
+                Uri photoUri = FileProvider.getUriForFile(this, "com.andrey.instapoo.fileprovider", photofile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        try {
-            super.onActivityResult(requestCode, resultCode, data);
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        Log.d("FUCK", "me cago en dios sí está entrando en la puta función");
+        if(resultCode == RESULT_OK) {
+            //super.onActivityResult(requestCode, resultCode, data);
+            //Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
             imageview.setImageBitmap(bitmap);
-        }catch(Exception e){}
+        }
     }
+
+    private File createImageFile() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
 
     private boolean checkAndRequestPermissions() {
         int permissionCAMERA = ContextCompat.checkSelfPermission(this,
@@ -67,7 +105,6 @@ public class MainActivity extends AppCompatActivity {
                     listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), PERMISSIONS_REQUEST_CODE);
             return false;
         }
-
         return true;
     }
 }
