8dffa5577f065dbeb7bae35b23eafa6e04aa2fbc
Andrey Torres Calderón <atorresf20@gmail.com>
2017-08-24 15:14:16 -0600

    Auto stash before merge of "AddToGallery" and "origin/AddToGallery"

---
+++ InstaPOO/app/src/main/java/com/andrey/instapoo/Filter.java
@@ -0,0 +1,35 @@
+package com.andrey.instapoo;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by andrey on 24/08/17.
+ */
+
+public class Filter extends MainActivity {
+}
